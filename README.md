6b78fc62b7bff9a40d74d5cffba75d4927ed2234
Brooks Benson <brooksbenson03@gmail.com>
2022-02-13 15:33:05 -0700

    database design, cities

---
+++ README.md
@@ -0,0 +1,12 @@
+# PostgreSQL
+
+## Database Design Process
+
+1. What kind of thing are we storing?
+2. What properties does this thing have?
+3. What *type* of data does each of those properties contain?
+
+## Lingo
+
+- Keywords: Tell the database that we want to do something. Always writtin in all uppercase.
+- Identifiers: Tell the database what thing we want to act on. Always writtin in all lowercase.
---
+++ cities.sql
@@ -0,0 +1,54 @@
+CREATE TABLE cities (
+  name VARCHAR(50),
+  country VARCHAR(50),
+  population INTEGER,
+  area INTEGER
+);
+-----------------------------------------------------
+INSERT INTO cities (name, country, population, area)
+VALUES ('Tokyo', 'Japan', 38505000, 8223);
+-----------------------------------------------------
+INSERT INTO cities (name, country, population, area)
+VALUES 
+  ('Delhi', 'India', 28125000, 2240),
+  ('Shanghai', 'China', 22125000, 4015),
+  ('Sao Paulo', 'Brazil', 20935000, 3043);
+-----------------------------------------------------
+SELECT name, country, population, area
+FROM cities;
+-----------------------------------------------------
+SELECT *
+FROM cities;
+-----------------------------------------------------
+SELECT name, population / area AS density 
+FROM cities;
+-----------------------------------------------------
+SELECT name || ', ' || country AS location
+FROM cities;
+-----------------------------------------------------
+SELECT CONCAT(name, ', ', country) AS location
+FROM cities;
+-----------------------------------------------------
+SELECT 
+  CONCAT(UPPER(name), ', ', UPPER(country)) AS location
+FROM 
+  cities;
+-----------------------------------------------------
+SELECT
+  UPPER(CONCAT(name, ', ', country)) AS location
+FROM
+  cities;
+-----------------------------------------------------
+SELECT
+  LOWER(CONCAT(name, ', ', country)) AS location
+FROM
+  cities;
+-----------------------------------------------------
+SELECT
+  LENGTH(CONCAT(name, ', ', country)) AS location
+FROM
+  cities;
+-----------------------------------------------------
+SELECT name, area
+FROM cities
+WHERE area > 4000;
